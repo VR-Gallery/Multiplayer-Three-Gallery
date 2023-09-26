99e9e544c2e949e00cf43199a6144e9946dcf1e1
Ray <62081739+SR0725@users.noreply.github.com>
2023-09-27 05:57:10 +0800

    feat: add husky

--- apps/discord_bot/src/cmd/deploy.sh
+++
@@ -1,7 +0,0 @@
-cd /project/VR-Room/
-
-git stash
-git pull
-git stash clear 
-
-sudo systemctl restart gallery
--- apps/discord_bot/src/models/BotCommand.ts
+++
@@ -1,4 +0,0 @@
-export default interface BotCommand {
-  name: string;
-  description: string;
-}
