280a616783274fb3a6ad1ecd0c6179bfa8132013
Ray <62081739+SR0725@users.noreply.github.com>
2023-09-27 06:29:17 +0800

    fix: cors

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,15 +3,17 @@ import { createServer } from "http";
 
 const port = Number(process.env.BACKEND_PORT) || 6000;
 
-const httpServer = createServer((req: any, res: any) => {
-  res.writeHead(200);
-  res.end("ok");
+const httpServer = createServer();
+
+httpServer.prependListener("request", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
 });
 
-const frontendUrl = process.env.FRONTEND_URL || "https://meta-gallery.art/";
+const frontendUrl = process.env.FRONTEND_URL || "https://meta-gallery.art";
 const io = new Server(httpServer, {
   cors: {
     origin: frontendUrl,
+    methods: ["GET", "POST"]
   },
 });
 
