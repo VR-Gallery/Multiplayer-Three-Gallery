6f7eb3f590d3cb48a9f3ff284e1b48c3c9c0e382
Ray <62081739+SR0725@users.noreply.github.com>
2023-09-27 04:31:09 +0800

    test: backend connection

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,7 +3,10 @@ import { createServer } from "http";
 
 const port = Number(process.env.BACKEND_PORT) || 6000;
 
-const httpServer = createServer();
+const httpServer = createServer((req: any, res: any) => {
+  res.writeHead(200);
+  res.end("socket.io");
+});
 
 const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
 const io = new Server(httpServer, {
