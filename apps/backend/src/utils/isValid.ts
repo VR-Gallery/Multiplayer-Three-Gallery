e55c315b562a7a2855dcd5f0396a5f68fb5e97d2
Ray <62081739+SR0725@users.noreply.github.com>
2023-09-27 01:51:13 +0800

    test

--- apps/backend/src/utils/isValid.ts
+++ apps/backend/src/utils/isValid.ts
@@ -1,10 +1,9 @@
 import { z } from "zod";
-import { log } from "logger";
 
 const isValid = (data: any, schema: z.ZodObject<any, any>) => {
   const validation = schema.safeParse(data);
   if (!validation.success) {
-    log(validation.error);
+    console.log(validation.error);
     return false;
   }
   return true;
