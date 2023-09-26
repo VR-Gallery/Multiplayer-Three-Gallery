ab7a3bb4deb9f19a98710763e8c9e40f2202049e
Ray <62081739+SR0725@users.noreply.github.com>
2023-09-26 23:10:53 +0800

    test: 測試 github action

--- apps/discord_bot/src/index.ts
+++ apps/discord_bot/src/index.ts
@@ -1,7 +1,7 @@
-import { registService, registCommands } from "./utils/bot";
-import GithubHandlerService from "./services/GithubHandlerService";
-import ChatGPTService from "./services/ChatGPTService";
+//import { registService, registCommands } from "./utils/bot";
+//import GithubHandlerService from "./services/GithubHandlerService";
+//import ChatGPTService from "./services/ChatGPTService";
 
-registService(ChatGPTService);
-registService(GithubHandlerService);
-registCommands();
+//registService(ChatGPTService);
+//registService(GithubHandlerService);
+//registCommands();
