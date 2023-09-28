9bee23f69e834af11ba73f47ce3b066b6b57166c
SR0725 <ray948787@gmail.com>
2023-09-28 16:23:37 +0800

    fix: modify ci file

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,11 +12,6 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 })
 
 const nextConfig = {
-  // uncomment the following snippet if using styled components
-  // compiler: {
-  //   styledComponents: true,
-  // },
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     reactRoot: 'concurrent',
     appDir: true,
